c47ef52c1e4788e5eb1acd5be148a88dc1ecb4c8
alicie1989123 <aliciabasilo.ab@gmail.com>
2022-01-12 22:56:05 -0600

    added-Cardano

---
+++ Cardano/Wallet.js
@@ -0,0 +1,9 @@
+import Loader from "./loader";
+
+export const addressBech32 = async () => {
+  await Loader.load();
+  const address = (await window.cardano.getUsedAddresses())[0];
+  return Loader.Cardano.Address.from_bytes(
+    Buffer.from(address, "hex")
+  ).to_bech32();
+};
